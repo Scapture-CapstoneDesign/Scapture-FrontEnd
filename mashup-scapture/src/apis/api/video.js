b902a8f866149d576c5eaed2ffc21de7fe8a7941
rtgc0915 <rtgc0915@gmail.com>
2024-05-31 00:39:20 +0900

    Update video.js

--- mashup-scapture/src/apis/api/video.js
+++ mashup-scapture/src/apis/api/video.js
@@ -1,11 +1,16 @@
 import { defualtInstance } from '../utils/instance';
+import axios from 'axios';
 
 export const getList = async () => {
     try {
-        const result = await defualtInstance.get('/api/video')
-        return result
-    }
-    catch (error) {
+        const result = await axios.get('/api/video', {
+            headers: {
+                Range: 'bytes=0-99999' // 0부터 9999 바이트 사이의 데이터 요청
+            }
+        });
+        return result;
+    } catch (error) {
         console.error('영상 데이터를 불러오는 중에 오류가 발생했습니다:', error);
     }
-}
+};
+
